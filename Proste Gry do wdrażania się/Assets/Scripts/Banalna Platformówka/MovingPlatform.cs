2bdbf67d65fc568c549853a327e24dbae72d2703
kathO2 <huberttylczak2002@gmail.com>
2025-08-06 11:09:43 +0200

    Dodana poruszająca się platforma + skrypt

---
+++ Proste Gry do wdrażania się/Assets/Scripts/Banalna Platformówka/MovingPlatform.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    [Range(1f, 5f)] public float moveSpeed = 2f;
+
+    Vector3 nextPosition;
+
+    void Start()
+    {
+        nextPosition = pointB.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
+
+        if (transform.position == nextPosition)
+        {
+            nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
